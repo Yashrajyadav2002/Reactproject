9b90d19013ef2575b38e79cbd2962e1fa7bccedb
yashrajyadav2002 <yyash9631@gmail.com>
2025-08-18 13:13:20 +0530

    project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import Signup from "./Components/Signup";
+// import Login from "./Components/Login";
+import Navbar from "./Components/Navbar";
+
+
+const App = () => {
+  return (
+    <>
+    {<Navbar/>}
+
+
+    {/* <Login/> */}
+    {/* <Signup/> */}
+  
+    
+{/*     
+    { <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes> } */}
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Signup.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Signup = ()=>{
+    let [signfrm,setSignfrm] = useState({})
+    let navigator = useNavigate()
+
+    function handleinput(e){
+        const {name,value} = e.target
+        setSignfrm({...signfrm,[name]:value})
+    }
+
+    function handlesubmit(e){
+        e.preventDefault()
+        localStorage.setItem("signfrm",JSON.stringify(signfrm))
+        navigator('/login')
+    }
+
+    return(
+
+        <>
+        <form action="" onSubmit={handlesubmit}>
+            <label htmlFor="">Name</label>
+            <input type="text" name="name"  onChange={handleinput}/><br /><br />
+
+            <label htmlFor="">Email</label>
+            <input type="text" name="email"  onChange={handleinput}/><br /><br />
+
+            <label htmlFor="">Password</label>
+            <input type="text" name="password"  onChange={handleinput}/><br /><br />
+
+            <input type="submit" />
+
+        </form>
+        
+        </>
+    )
+}
+export default Signup
